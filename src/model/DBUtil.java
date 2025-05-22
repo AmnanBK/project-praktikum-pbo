d550a8bf859c879d9f7227be85f92ea7d21c0e67
Amnan <amnanbudikusuma@gmail.com>
2025-05-22 13:47:52 +0700

    feat: add DBUtil for connection

---
+++ src/model/DBUtil.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author amnan
+ */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBUtil {
+        private static final String DB_URL = "jdbc:mysql://localhost:3306/hotel_reservation_db?useSSL=false&serverTimezone=UTC";
+    private static final String DB_USER = "root";
+    private static final String DB_PASS = "";
+
+    public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("MySQL JDBC Driver not found", e);
+        }
+
+        return DriverManager.getConnection(DB_URL, DB_USER, DB_PASS);
+    }
+}
